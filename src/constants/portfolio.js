8df304606147cccf758329171165052bd289d6bc
Sebastian Uddén <sebastian.udden@nobia.com>
2020-11-01 14:31:27 +0100

    refactor: change caps

--- src/constants/portfolio.js
+++ src/constants/portfolio.js
@@ -5,7 +5,7 @@ const { alexander, sebastian } = B1TF0UNDERS;
 export const PORTFOLIO = [
   {
     title: "Galactic Clock",
-    description: "It's both time AND space!",
+    description: "It's both TIME and SPACE!",
     href: "https://galactic-clock.netlify.app/",
     img:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/Eopsaltria_australis_-_Mogo_Campground.jpg/500px-Eopsaltria_australis_-_Mogo_Campground.jpg",
