3e29a47c5674c42abc069e23fa6e3f9888f14deb
Sebastian Uddén <sebastian.udden@nobia.com>
2020-11-02 17:07:48 +0100

    feat: new gists

--- src/constants/gists.js
+++ src/constants/gists.js
@@ -10,4 +10,28 @@ export const GISTS = [
       "https://gist.github.com/SebastianUdden/9843533963adcb2e473e17ded05d4881",
     authors: [sebastian],
   },
+  {
+    title: "Create a basic styled component",
+    description:
+      "How to setup a basic styled-component inside a create-react-app.",
+    href:
+      "https://gist.github.com/SebastianUdden/996d7a03a6687dc3f0059a3e538c3c23",
+    authors: [sebastian],
+  },
+  {
+    title: "Setup icons and metadata",
+    description:
+      "Explains how to improve SEO and user experience with some small changes to metadata and icons.",
+    href:
+      "https://gist.github.com/SebastianUdden/c9dd565e6836a476c3a640e8773f3cd6",
+    authors: [sebastian],
+  },
+  {
+    title: "Creating a gist",
+    description:
+      "How to create a gist from scratch. TLDR; it's really easy so just do it!",
+    href:
+      "https://gist.github.com/SebastianUdden/0b3916a2be7754329174afdbc6acd29b",
+    authors: [sebastian],
+  },
 ];
